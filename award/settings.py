8e56b2d097348d76e0b86fa6781c5238b8020630
Kenneth-joseph <josephochieng812@gmail.com>
2020-02-15 21:59:13 +0300

    edited my settings and urls

--- award/settings.py
+++ award/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'folio.apps.FolioConfig',
+    'bootstrap4',
+
 ]
 
 MIDDLEWARE = [
--- folio/views.py
+++ folio/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404, HttpRequest, HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
