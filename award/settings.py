936fa0962ac0239bc1becf0d3b40c81803c5c084
Kenneth-joseph <josephochieng812@gmail.com>
2020-02-15 23:40:48 +0300

    added my first models

--- award/settings.py
+++ award/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'dvlhel18)a@qj0(ce_hv37vqb+i@lbd9tp!wz0gmw#7)z+ibd4'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,17 +70,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'award.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myport',
+        'USER': 'postgres',
+        'PASSWORD': 'kent1234',
+        'HOST': 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -102,13 +101,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -116,8 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
--- folio/models.py
+++ folio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=30)
+    bio = models.TextField()
+    phone_number = models.IntegerField(blank=True)
+    email =models.EmailField()
 
 # Create your models here.
