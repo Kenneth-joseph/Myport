3e14fd36377c734b1a1eacc1ae1b84abfd644472
Kenneth-joseph <josephochieng812@gmail.com>
2020-02-16 17:48:05 +0300

    made changes in my models and navbar

--- folio/models.py
+++ folio/models.py
@@ -4,9 +4,11 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    users = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     bio = models.TextField()
     phone_number = models.IntegerField(blank=True)
+    profile_pic = models.ImageField(upload_to='pictures/',default='kent.jpg')
     email = models.EmailField()
 
     def __str__(self):
@@ -18,8 +20,24 @@ class Project(models.Model):
     description = models.TextField(max_length=100)
     project_link = models.CharField(max_length=100)
     profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    project_pic = models.ImageField(upload_to='pictures/', default='kent.jpg')
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title
 
 
 class Rating(models.Model):
-    design = models.CharField(max_length=30)
+    design = models.IntegerField(max_length=30, default=0)
+    usability = models.IntegerField(max_length=30, default=0)
+    content = models.IntegerField(max_length=30, default=0)
+    project = models.ForeignKey(Project)
+
+    def __str__(self):
+        return self.content
 # Create your models here.
