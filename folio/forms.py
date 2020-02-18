784626fc99cfae98e3c1cb56b7e204538f69b41f
Kenneth-joseph <josephochieng812@gmail.com>
2020-02-18 12:16:57 +0300

    added my new project function

---
+++ folio/forms.py
@@ -0,0 +1,8 @@
+from .models import Project
+from django import forms
+
+
+class NewProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['profile']
--- folio/views.py
+++ folio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import Http404, HttpRequest, HttpResponse
 from .models import Project, Profile, Rating
+from .forms import NewProjectForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -9,6 +11,7 @@ def home(request):
     return render(request, 'home.html', {"project": project})
 
 
+@login_required(login_url='/accounts/login/')
 def search_results(request):
     if 'project' in request.GET and request.GET['project']:
         search_term = request.GET.get("project")
@@ -17,4 +20,19 @@ def search_results(request):
         return render(request, 'search.html', {"message": message, "projects": searched_project})
     else:
         message = "You have made any search yet, please do to get the awesome projects"
-        return render(request, 'search.html', {"message":message})
+        return render(request, 'search.html', {"message": message})
+
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.profile = current_user
+            project.save()
+        return redirect('home')
+    else:
+        form = NewProjectForm
+    return render(request, 'new_project.html', {"form": form})
